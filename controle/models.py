dcc6cc7384e10c3940296dd3853d1ed1a2589974
pmela <pmelaeduarda2015@gmail.com>
2021-02-25 14:21:39 -0300

    Models Atualizada.

--- controle/models.py
+++ controle/models.py
@@ -3,27 +3,42 @@ from django.db import models
 
 # Create your models here.
 
+class Categoria(models.Model):
+    descricao = models.CharField(max_length=32)
+
+    def __str__(self):
+        return self.descricao
+
+
+class Situacao(models.Model):
+    descricao = models.CharField(max_length=32)
+
+    def __str__(self):
+        return self.descricao
+
 
 class Entradas(models.Model):
     data = models.DateField()
     descricao = models.CharField(max_length=32)
-    situacao = models.CharField(max_length=12)
     quantidade_de_x_dividido = models.FloatField()
     valor_mensal = models.FloatField()
     valor_total = models.FloatField()
+    categoriaentrada = models.ForeignKey(Categoria, null=True, on_delete=models.PROTECT)
+    situacaoentrada = models.ForeignKey(Situacao, null=True, on_delete=models.PROTECT)
 
     def __str__(self):
-     return self.descricao
+        return self.descricao
 
 
 class Saidas(models.Model):
     data = models.DateField()
     descricao = models.CharField(max_length=32)
-    situacao = models.CharField(max_length=12)
     nome_dividendo = models.CharField(max_length=12)
     quantidade_de_x_dividido = models.FloatField()
     valor_mensal = models.FloatField()
     valor_total = models.FloatField()
+    categoriasaida = models.ForeignKey(Categoria, null=True, on_delete=models.PROTECT)
+    situacaosaida = models.ForeignKey(Situacao, null=True, on_delete=models.PROTECT)
 
     def __str__(self):
-     return self.descricao
+        return self.descricao
